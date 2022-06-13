4a3cf38250d96bbaf1adaa514cebdd22c30329c3
Timibreez <ffftimi@gmail.com>
2022-06-14 00:16:40 +0100

    conditional statements

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-a = '15' + 2
+"""Conditional Statments Practice"""
+points = 174
+
+if points <= 50:
+    result = "Congratulations! You won a wooden rabbit!"
+elif points <= 150:
+    result = "Oh dear, no prize this time."
+elif points <= 180:
+    result = "Congratulations! You won a wafer-thin mint!"
+else:
+    result = "Congratulations! You won a penguin!"
+
+print(result)
